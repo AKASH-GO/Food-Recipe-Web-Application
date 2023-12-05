219edeb87d8f4d2fa35b96348d981102ec5c0a9a
AKASH <88474098+AKASH-GO@users.noreply.github.com>
2023-12-05 17:30:05 +0530

    Recipe app

--- script.js
+++ script.js
@@ -1,122 +1,114 @@
-const searchInput = document.getElementById("searchInput");
-const searchButton = document.getElementById("searchButton");
-const mealList = document.getElementById("mealList");
-const modalContainer = document.getElementById("modal-container");
-const mealDetailsContent = document.getElementById("meal-details-content");
-const recipeCloseBtn = document.getElementById("recipeCloseBtn");
+const searchInput = document.getElementById('searchInput');
+const searchButton = document.getElementById('searchButton');
+const mealList = document.getElementById('mealList');
+const modalContainer = document.querySelector('.modal-container');
+const mealDetailsContent = document.querySelector('.meal-details-content');
+const recipeCloseBtn = document.getElementById('recipeCloseBtn');
 
-// Event Listeners
-searchButton.addEventListener("click", async () => {
-  const ingredient = searchInput.value.trim();
-  if (ingredient) {
-    const meals = await searchMealsByIngredient(ingredient);
-    displayMeals(meals);
-  }
-});
-
-mealList.addEventListener("click", async (e) => {
-  const card = e.target.closest(".meal-item");
-  if (card) {
-    const mealId = card.dataset.id;
-    const meal = await getMealDetails(mealId);
-    if (meal) {
-      showMealDeatilsPopup(meal);
+// Event listeners
+searchButton.addEventListener('click', async () => {
+    const ingredient = searchInput.value.trim();
+    if (ingredient) {
+        const meals = await searchMealsByIngredient(ingredient);
+        displayMeals(meals);
     }
-  }
 });
 
-// Function used to fetch teh meals by ingredients
+mealList.addEventListener('click', async (e) => {
+    const card = e.target.closest('.meal-item');
+    if (card) {
+        const mealId = card.dataset.id;
+        const meal = await getMealDetails(mealId);
+        if (meal) {
+            showMealDetailsPopup(meal);
+        }
+    }
+});
+
+// Function to fetch meals by ingredient
 async function searchMealsByIngredient(ingredient) {
-  try {
-    const response = await fetch(
-      `https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`
-    );
-    const data = await response.json();
-    return data.meals;
-  } catch (err) {
-    // Show thw error in the console
-    console.error("Error fetching data:", err);
-  }
+    try {
+        const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`);
+        const data = await response.json();
+        return data.meals;
+    } catch (error) {
+        // Show error in console
+        console.error('Error fetching data:', error);
+    }
 }
 
 // Function to fetch meal details by ID
-async function getMealDeatils(mealId) {
-  try {
-    const response = await fetch(
-      `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`
-    );
-    const data = await response.json();
-    return data.meals[0];
-  } catch (err) {
-    console.error("Error fetching meal details:", err);
-  }
+async function getMealDetails(mealId) {
+    try {
+        const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`);
+        const data = await response.json();
+        return data.meals[0];
+    } catch (error) {
+        console.error('Error fetching meal details:', error);
+    }
 }
 
-// Function to display the meals in list
+// Function to display meals in the list
 function displayMeals(meals) {
-  if (meals) {
-    meals.forEach((meal) => {
-      const mealItem = document.createElement("div");
-      mealItem.classList.add("meal-item");
-      mealItem.dataset.id = meal.idMeal;
-      mealItem.innerHTML = `
+    mealList.innerHTML = '';
+    if (meals) {
+        meals.forEach((meal) => {
+            const mealItem = document.createElement('div');
+            mealItem.classList.add('meal-item');
+            mealItem.dataset.id = meal.idMeal;
+            mealItem.innerHTML = `
                 <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
-                <h3>${meal.strMeal}</h3>`;
-      mealList.appendChild(mealItem);
-    });
-  } else {
-    mealList.innerHTML = `
-            <p>No meals was found. Try any another ingredient</p>
+                <h3>${meal.strMeal}</h3>
             `;
-  }
+            mealList.appendChild(mealItem);
+        });
+    } else {
+        mealList.innerHTML = '<p>No meals found. Try another ingredient.</p>';
+    }
 }
 
-// Function to show the meal details popup
-function showMealDeatilsPopup(meal) {
-  // clean it up
-  mealDetailsContent.innerHTML = `
-    <h2 class="recipe-title">${meal.strMeal}</h2>
-    <p class="recipe-category">${meal.strCategory}</p>
-    <div class="recipe-instruct">
-        <h3>Instructions:</h3>
-        <p>${meal.strInstructions}</p>
-    </div>
-    <div class="recipe-meal-img">
-        <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
-    </div>
-    <div class="recipe-link">
-        <a href="${meal.strYoutube}" target="_blank">Watch Video</a>
-    </div>
+// Function to create and display meal details on popup
+function showMealDetailsPopup(meal) {
+    mealDetailsContent.innerHTML = `
+        <h2 class="recipe-title">${meal.strMeal}</h2>
+        <p class="recipe-category">${meal.strCategory}</p>
+        <div class="recipe-instruct">
+            <h3>Instructions:</h3>
+            <p>${meal.strInstructions}</p>
+        </div>
+        <div class="recipe-img">
+            <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
+        </div>
+        <div class="recipe-video">
+            <a href="${meal.strYoutube}" target="_blank">Video Tutorial</a>
+        </div>
     `;
-  modalContainer.style.display = "block";
+    modalContainer.style.display = 'block';
 }
 
 // Event listener for popup close button
-recipeCloseBtn.addEventListener("click", closeRecipeModal);
+recipeCloseBtn.addEventListener('click', closeRecipeModal);
 
-// Function to close the popup
 function closeRecipeModal() {
-  modalContainer.style.display = "none";
+    modalContainer.style.display = 'none';
 }
 
-// Event listener for outside click
-searchInput.addEventListener("keyup", (e) => {
-  if (e.key === "Enter") {
-    performSearch();
-  }
+searchInput.addEventListener('keyup', (e) => {
+    if (e.key === 'Enter') {
+        performSearch();
+    }
 });
 
-// Function to perform the search
 async function performSearch() {
-  const ingredient = searchInput.value.trim();
-  if (ingredient) {
-    const meals = await searchMealsByIngredient(ingredient);
-    displayMeals(meals);
-  }
+    const ingredient = searchInput.value.trim();
+    if (ingredient) {
+        const meals = await searchMealsByIngredient(ingredient);
+        displayMeals(meals);
+    }
 }
 
 // Perform a chicken search on page load
-window.addEventListener('load',()=>{
-    searchInput.value='chicken';
+window.addEventListener('load', () => {
+    searchInput.value = 'chicken';
     performSearch();
-})
+});
